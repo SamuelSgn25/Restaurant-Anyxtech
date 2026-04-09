a572e4471f32a053be53ceb3721d7addef8e04bc
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-09 13:11:05 +0100

    Add backend app module

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseConfig } from './config/database.config';
+import { EventsModule } from './events/events.module';
+import { MenuModule } from './menu/menu.module';
+import { ReservationsModule } from './reservations/reservations.module';
+import { SettingsModule } from './settings/settings.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: databaseConfig
+    }),
+    SettingsModule,
+    MenuModule,
+    EventsModule,
+    ReservationsModule
+  ]
+})
+export class AppModule {}
