f0ca77f767c38c3b1d032e1382feab2af93c6bfd
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-09 13:06:53 +0100

    Create events service

---
+++ apps/api/src/events/events.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class EventsService {
+  getEvents() {
+    return [
+      {
+        id: 'brunch-tropical',
+        title: 'Brunch tropical du dimanche',
+        enabled: true,
+        cadence: 'weekly'
+      },
+      {
+        id: 'chef-table',
+        title: 'Chef table degustation',
+        enabled: true,
+        cadence: 'monthly'
+      }
+    ];
+  }
+}
