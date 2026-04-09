62985135d65dc8f1eb4354e1246b5fefb11265e9
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-09 13:16:37 +0100

    Add configuration for database

---
+++ apps/api/src/config/database.config.ts
@@ -0,0 +1,12 @@
+export function databaseConfig() {
+  return {
+    type: 'postgres' as const,
+    host: process.env.DB_HOST ?? 'localhost',
+    port: Number(process.env.DB_PORT ?? 5432),
+    username: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    database: process.env.DB_NAME ?? 'hotel_cactus_restaurant',
+    autoLoadEntities: true,
+    synchronize: true
+  };
+}
