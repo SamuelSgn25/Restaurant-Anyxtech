eac8d3f2f2ee91e5fb80ff9520e096320bf09347
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-10 09:34:26 +0100

    Add module for staff users

---
+++ apps/api/src/staff/staff.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { Roles } from '../auth/auth.decorators';
+import { RestaurantDataService } from '../data/restaurant-data.service';
+
+@Controller('staff')
+export class StaffController {
+  constructor(private readonly restaurantDataService: RestaurantDataService) {}
+
+  @Roles('super_admin', 'admin')
+  @Get()
+  findAll() {
+    return this.restaurantDataService.getPublicUsers();
+  }
+}
