813aa3d49d1e81602e2d3460a7e34880295d4daf
SamuelSgn25 <soglohounsamuel2@gmail.com>
2026-04-14 09:51:24 +0100

    Implementing some modifications on events module

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
+import { EventsGateway } from './events.gateway';
 
 @Module({
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService, EventsGateway],
+  exports: [EventsGateway]
 })
 export class EventsModule {}
