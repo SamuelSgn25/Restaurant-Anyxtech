58fea353db3c0037388fbdd197bd693fd7140ed0
SamuelSgn <samuel.soglohoun@epitech.eu>
2026-04-15 09:23:47 +0100

    Add modification on menu unit tests

--- apps/api/src/menu/menu.service.spec.ts
+++ apps/api/src/menu/menu.service.spec.ts
@@ -2,8 +2,10 @@ import { RestaurantDataService } from '../data/restaurant-data.service';
 import { MenuService } from './menu.service';
 
 describe('MenuService', () => {
+  const mockEventsGateway = { broadcast: jest.fn() } as any;
+
   it('returns menu categories', () => {
-    const service = new MenuService(new RestaurantDataService());
+    const service = new MenuService(new RestaurantDataService(mockEventsGateway));
     const categories = service.getMenu();
 
     expect(categories.length).toBeGreaterThan(1);
